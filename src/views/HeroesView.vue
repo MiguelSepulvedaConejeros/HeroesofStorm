0b64fd14f5420fe5aac22fedb93337b7c5426ff8
MiguelSepulvedaConejeros <msepco@gmail.com>
2024-12-16 19:12:46 -0300

    modif_heroesview.vue

--- src/views/HeroesView.vue
+++ src/views/HeroesView.vue
@@ -15,7 +15,7 @@
         </v-row>
         <v-row class="content-heroes">
             <div class="table-container">
-                <div class="full-width-table">
+                <table class="full-width-table">
                     <thead>
                         <tr>
                             <th>Heroes</th>
@@ -34,7 +34,7 @@
                             <td>{{ hero.attack }}</td>
                         </tr>
                     </tbody>
-                </div>
+                </table>
             </div>
         </v-row>
     </v-container>
